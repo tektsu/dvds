c929de20b0d49e3a258c095f168bd0636c5bd99e
Steve Baker <tektsu@gmail.com>
2014-04-13 12:52:15 -0700

    Add Dvd class

---
+++ src/main/java/com/xecko/util/dvds/Dvd.java
@@ -0,0 +1,67 @@
+package com.xecko.util.dvds;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import com.xecko.util.dvds.Entry;
+
+/**
+ * @author steve
+ * 
+ */
+public class Dvd {
+	ArrayList<Entry> entries; // The entries in this chunk
+	long size; // The space taken by these directories
+	long maxSize; // The maximum space which may be used
+	String name; // The name of the chunk
+
+	public Dvd(String name) {
+		this.name = name;
+		entries = new ArrayList<Entry>();
+		size = 0;
+		setMaxSizeToDVD();
+	}
+
+	public void dump() {
+		System.out.println("Name: " + name + " ( " + size + ")");
+		for (Entry entry : entries) {
+			entry.dump();
+		}
+		System.out.println("");
+	}
+
+	public void copy(File destinationDirectory) throws IOException {
+		File destination = new File(destinationDirectory.getAbsolutePath() + File.separator + name);
+		for (Entry entry : entries) {
+			entry.copy(destination);
+		}
+	}
+
+	public void setMaxSize(long size) {
+		maxSize = size;
+	}
+
+	public long getMaxSize() {
+		return maxSize;
+	}
+
+	public void setMaxSizeToDVD() {
+		setMaxSize((long)(4.2 * 1024 * 1024 * 1024));
+	}
+
+	public long size() {
+		return size;
+	}
+
+	public void add(Entry entry) throws IOException {
+		if (size + entry.size() > maxSize) {
+			throw new IOException("No room");
+		}
+		entries.add(entry);
+		size += entry.size();
+	}
+
+	public int getEntryCount() {
+		return entries.size();
+	}
+}
---
+++ src/test/java/com/xecko/util/dvds/DvdTest.java
@@ -0,0 +1,78 @@
+package com.xecko.util.dvds;
+
+import com.xecko.util.dvds.Entry;
+import com.xecko.util.dvds.Dvd;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+import org.apache.commons.io.FileUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class DvdTest {
+	private Path tmp = null;
+	
+	@BeforeClass
+	public void createTempFileSystem() throws IOException {
+		tmp = Files.createTempDirectory("__EntryTest__");
+		Files.createDirectory(Paths.get(tmp + "/destination"));
+		Files.createDirectory(Paths.get(tmp + "/source"));
+		Files.createDirectory(Paths.get(tmp + "/source/emptydir"));
+		Files.createDirectory(Paths.get(tmp + "/source/dir1"));
+		RandomAccessFile fh = new RandomAccessFile(tmp + "/source/dir1/file1", "rw");
+		fh.setLength(1024);
+		fh.close();
+		fh = new RandomAccessFile(tmp + "/source/file2", "rw");
+		fh.setLength(1024 * 6);
+		fh.close();
+	}
+	
+	@Test
+	public void maxSizeTest() {
+		Dvd dvd = new Dvd("DVD");
+		Assert.assertEquals(dvd.getMaxSize(), (long)(4.2 * 1024 * 1024 * 1024));
+		dvd.setMaxSize(10);
+		Assert.assertEquals(dvd.getMaxSize(), 10);
+	}
+	
+	@Test
+	public void addEntryTest() throws IOException {
+		Dvd dvd = new Dvd("DVD");
+		dvd.add(new Entry(tmp + "/source/dir1"));
+		Assert.assertEquals(dvd.size(), 1024);
+		dvd.add(new Entry(tmp + "/source/file2"));
+		Assert.assertEquals(dvd.size(), 7 * 1024);
+		Assert.assertEquals(dvd.getEntryCount(), 2);
+	}
+    
+	@Test(expectedExceptions = IOException.class)
+	public void overflowTest() throws IOException {
+		Dvd dvd = new Dvd("DVD");
+		dvd.setMaxSize(2 * 1024);
+		dvd.add(new Entry(tmp + "/source/file2"));
+	}
+	
+	@Test
+	public void copyTest() throws IOException {
+		Dvd dvd = new Dvd("DVD");
+		dvd.add(new Entry(tmp + "/source/dir1"));
+		dvd.add(new Entry(tmp + "/source/file2"));
+		dvd.copy(new File(tmp + "/destination"));
+    	Entry dirEntry = new Entry(tmp + "/destination/DVD/dir1");
+    	Assert.assertEquals(dirEntry.size(), 1024);
+    	Entry fileEntry = new Entry(tmp + "/destination/DVD/file2");
+    	Assert.assertEquals(fileEntry.size(), 1024 * 6);
+	}
+    
+    @AfterClass
+    public void removeTempFileSystem() throws IOException {
+    	FileUtils.deleteDirectory(tmp.toFile());
+    }
+}
