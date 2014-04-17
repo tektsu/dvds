65c01480302ec773b245bd8110aa998e1bd7dab9
Steve Baker <tektsu@gmail.com>
2014-04-16 18:11:52 -0700

    Test for getContents()

--- src/test/java/com/xecko/util/dvds/SourceTest.java
+++ src/test/java/com/xecko/util/dvds/SourceTest.java
@@ -3,8 +3,10 @@ package com.xecko.util.dvds;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -30,6 +32,16 @@ public class SourceTest {
 	@Test(expectedExceptions = FileNotFoundException.class)
 	public void setFileSource() throws FileNotFoundException {
 		new Source(tmp + "/source/dir1/file1");
+	}
+	
+	@Test
+	public void checkContents() throws IOException {
+		Source source = new Source(tmp + "/source");
+		ArrayList<Entry> entries = source.getContents();
+		Assert.assertEquals(entries.size(), 3);
+		Assert.assertEquals(entries.get(0).getName(), "file2");
+		Assert.assertEquals(entries.get(1).getName(), "dir1");
+		Assert.assertEquals(entries.get(2).getName(), "emptydir");
 	}
 	
     @AfterClass
